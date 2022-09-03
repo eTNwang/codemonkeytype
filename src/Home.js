7931e5761f4f39d2ad03f0c5e6d00cce67970c36
exyzhao <ethanxyzhao@gmail.com>
2022-09-03 13:38:26 -0400

    Highlight active character

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,34 @@
 import React, { useState } from "react"
 
-const wordList = ["array", "word", "state", "type", "tester"]
+// Each solution is stored as an array of strings, with each string representing one line of the solution
+const lineList = ["ab cde", "fgh", "i jklm"]
 
 const Home = () => {
-  const [typed, setTyped] = useState("")
+  // Store an array of booleans depending on if the character was typed correctly
+  const [isCharacterCorrect, setIsCharacterCorrect] = useState(Array(lineList.length))
+  const [currLineIndex, setCurrLineIndex] = useState(0)
+  const [currCharIndex, setCurrCharIndex] = useState(0)
+
+  // To access characters, use lineList[line #][character #]
 
   return (
     <>
       <h1>Code Monkey Type</h1>
 
-      {/* Map list of words out to individual characters */}
+      {/* Map list of lines out to individual characters */}
+
       <div className="test">
-        {wordList.map((word) => {
+        {lineList.map((line, lineIndex) => {
           return (
-            <div>
-              {word.split("").map((char) => {
+            <div key={lineIndex}>
+              {line.split("").map((char, charIndex) => {
                 return (
-                  <span>{char}</span>
+                  <span
+                    className={lineIndex === currLineIndex && charIndex === currCharIndex ? "active" : "inactive"}
+                    key={[lineIndex, charIndex]}
+                  >
+                    {char}
+                  </span>
                 )
               })}
             </div>
