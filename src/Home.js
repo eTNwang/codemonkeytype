1629ada4dda701fc5ff40516ffa2195dce62c29b
exyzhao <ethanxyzhao@gmail.com>
2022-09-03 11:50:47 -0400

    Build word array into characters

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react"
 
+const wordList = ["array", "word", "state", "type", "tester"]
+
 const Home = () => {
-  const [textToType, setTextToType] = useState("this website is made for typing practice")
   const [typed, setTyped] = useState("")
 
   return (
     <>
       <h1>Code Monkey Type</h1>
-      <p className="text-to-type">type a bunch of words here this is where the sentences go</p>
-      <input
-        placeholder="placeholder text"
-        value={typed}
-        onChange={({ target }) => setTyped(target.value)}
-      />
+
+      {/* Map list of words out to individual characters */}
+      <div className="test">
+        {wordList.map((word) => {
+          return (
+            <div>
+              {word.split("").map((char) => {
+                return (
+                  <span>{char}</span>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
