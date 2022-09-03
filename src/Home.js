236f67ee923dddeed7de71dede0629cd6ece1097
exyzhao <ethanxyzhao@gmail.com>
2022-09-03 11:25:43 -0400

    Add input box

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,18 @@
+import React, { useState } from "react"
+
 const Home = () => {
+  const [textToType, setTextToType] = useState("this website is made for typing practice")
+  const [typed, setTyped] = useState("")
+
   return (
     <>
       <h1>Code Monkey Type</h1>
       <p className="text-to-type">type a bunch of words here this is where the sentences go</p>
+      <input
+        placeholder="placeholder text"
+        value={typed}
+        onChange={({ target }) => setTyped(target.value)}
+      />
     </>
   )
 }
